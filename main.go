b74a8e0bc8b8f53071ca682aed4e61cb45d3bf36
wgeorge1990 <williameverettgeorge@icloud.com>
2019-11-16 12:17:11 -0500

    Updated data structure that is passed into tpl.gohtml, next is edit handling of data in template

--- main.go
+++ main.go
@@ -5,33 +5,54 @@ import (
 	"log"
 	"os"
 	"text/template"
+	"time"
 )
 
+var todos  = []string{
+	"Go sky diving",
+	"Dont worry be happy",
+	"Go deep sea diving",
+	"Go to Santorini",
+	"Bleach my hair",
+	"Buy a race car",
+	"Travel to the Netherlands",
+	"Thru hike the Appalachian trail",
+	"Go on a safari",
+	"Learn  to fly a plane",
+
+}
+
+type templateData struct{
+	Todos []string
+	MetaData pageVariables
+}
+
+type pageVariables struct{
+	Date string
+	Time string
+}
+
+var now = time.Now()
+var homePageVars = pageVariables{
+	Date: now.Format("02-01-2006"),
+	Time: now.Format("15:04:05"),
+}
+
+var appData = templateData{
+	Todos: todos,
+	MetaData: homePageVars,
+}
 
 
 func main() {
+	fmt.Println(appData.MetaData.Date)
 	//tpl returns pointer to a template if no err
 	tpl, err := template.ParseFiles("temps/tpl.gohtml", "temps/footnote.gohtml", "temps/header.gohtml")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	todos := []string{
-		"Go sky diving",
-		"Dont worry be happy",
-		"Go deep sea diving",
-		"Go to Santorini",
-		"Bleach my hair",
-		"Buy a race car",
-		"Travel to the Netherlands",
-		"Thru hike the Appalachian trail",
-		"Go on a safari",
-		"Learn  to fly a plane",
-
-	}
-
 	container := []interface{}{ todos}
-	fmt.Println(container)
 	//which then's gives us the ability to call Execute!!
 	nf, err := os.Create("index.html")
 	if err != nil {
